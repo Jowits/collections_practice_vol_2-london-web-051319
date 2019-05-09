dba5248749e1d8024bb14d6bfb133cf45d61d569
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-09 09:36:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,3 +30,12 @@ def count_elements(array)
   array.each {|i2| if i2 == i then count += 1 end}
   i[:count] = count}
 end
+
+def merge_data(keys, data)
+  merged = []
+  keys.each {|i| data.first.map {|k, v|
+    if i.values[0] == k
+      then merged << i.merge(v)
+  end
+  }}
+end
