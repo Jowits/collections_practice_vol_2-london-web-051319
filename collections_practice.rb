359f0b9d690b902251ab958346470ed14a611506
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-09 09:26:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,8 +3,12 @@ require 'pry'
 
 def begins_with_r(element)
     element.all? do |string|
-    string[0] == "r"
+      string[0] == "r"
   end
 end
 
 def contain_a(element)
+  element.select do |string|
+    string.include?("a")
+  end
+end
