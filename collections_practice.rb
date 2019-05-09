e0813989a04521c84a72abb056e9f41010224e4c
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-09 09:25:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,10 @@
 # your code goes here
 require 'pry'
 
-def begins_with_r(arr)
-    arr.all? do |string|
+def begins_with_r(element)
+    element.all? do |string|
     string[0] == "r"
   end
 end
+
+def contain_a(element)
