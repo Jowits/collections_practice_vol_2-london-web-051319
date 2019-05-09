d21d52441c78f83217483ae3f1327afa984e3247
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-09 09:20:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,14 @@
 # your code goes here
 def begins_with_r(element)
-  if element.start_with?("r")
-     true
-  else
-     false
+  i = 0
+  while i < element.length
+    all = element.all?
+    if element[i].start_with?("r")
+      puts true
+      i += 1
+    else
+      puts false
+      i += 1
+    end
   end
-end
+end  
