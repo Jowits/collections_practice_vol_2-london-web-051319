45fa831dff9580719431eb8fb815af68e785ac41
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-09 09:24:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,16 +1,8 @@
 # your code goes here
-element = ["rat", "fang", true, "rat"]
+require 'pry'
 
-def begins_with_r(element)
-  i = 0
-  while i < element.length
-  all = element.all?
-    if element[i].start_with?("r")
-      puts true
-      i += 1
-    else
-      puts false
-      i += 1
-    end
+ def begins_with_r(arr)
+    arr.all? do |string|
+    string[0] == "r"
   end
 end
